04306ff7eb47aafe5d58d9173e8d33d34936387b
MD <mayankanildwivedi@gmail.com>
2024-04-12 21:34:19 +0530

    Altered

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,14 @@
 module.exports = {
     apps : [{
       name: 'amruit-backend',
-      script: 'yarn',
+      script: 'npm',
       args: 'start:dev',
-      interpreter: '/bin/bash', // You may need to adjust this path depending on your system
       env: {
-        NODE_ENV: 'development'
+        NODE_ENV: 'production'
       },
-      watch: true, // Enable watching for file changes
+      watch: false, // Enable watching for file changes
       ignore_watch: ['node_modules'], // Ignore watching changes in node_modules directory
-      watch_delay: 1000,
-      log_date_format: 'YYYY-MM-DD HH:mm:ss',
-      combine_logs: true,
-      merge_logs: true,
-      error_file: 'logs/pm2_error.log',
-      out_file: 'logs/pm2_out.log',
-      pid_file: 'logs/pm2.pid'
+      watch_delay: 1000
     }]
   };
   
